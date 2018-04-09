f85436b00eee9543fcc4c99a6cdbb5eee11b3aa7
Fil8 <filippo.maccagni@gmail.com>
2018-04-09 22:32:15 +0200

    removed files

--- sub_cont.py
+++ sub_cont.py
@@ -1,20 +1,21 @@
 import stimela
 
-INPUT="/home/maccagni/FornaxA/data/MeerKAT/fa1/input"
-OUTPUT="/home/maccagni/FornaxA/data/MeerKAT/fa1/output"
-MSDIR="/home/maccagni/FornaxA/data/MeerKAT/fa1/msdir"
+INPUT="/Users/maccagni/Projects/FornaX/FornaxA/data/MeerKAT/fa1/input"
+OUTPUT="/Users/maccagni/Projects/FornaX/FornaxA/data/MeerKAT/fa1/output"
+MSDIR="/Users/maccagni/Projects/FornaX/FornaxA/data/MeerKAT/fa1/msdir"
 
-ms = MSDIR + '/fa1_conc_csub-corr.ms'
+ms = MSDIR + '/fa1_tosub.ms'
 toutname = MSDIR + '/fa1_conc_tmp-corr.ms'
 recipe = stimela.Recipe("Subtract model of continuum", ms_dir=MSDIR)
 
 recipe.add("cab/msutils", "sub_model",
 {
-     "command"	  : 'addcol',
-     "msname"      : 'fa1_conc_csub-corr.ms', 
- 	"subtract"    : True, 
+    "command"	  : 'sumcols',
+    "msname"      : 'fa1_conc_csub-corr.ms', 
+ 	#"subtract"    : True, 
  	"col1" 		  : 'CORRECTED_DATA',
  	"col2"        : 'MODEL_DATA',
+ 	"tocol"		  : 'CORRECTED_DATA'
 },
 input=INPUT, 
 output=OUTPUT,
@@ -23,32 +24,32 @@ label="Subtract model_data column")
 recipe.run()
 
 
-import pyrap.tables as tables
+#import pyrap.tables as tables
 
 
 
 
-print ''
-print '--- Working on file {0:s} ---'.format(ms)
+#print ''
+#print '--- Working on file {0:s} ---'.format(ms)
 
-t=tables.table(ms)
-tout = t.copy(toutname)
-tout.close()
-t.close()
-t=tables.table(toutname, readonly = False)
+#t=tables.table(ms)
+#tout = t.copy(toutname)
+#tout.close()
+#t.close()
+#t=tables.table(toutname, readonly = False)
 
-model = t.getcol('MODEL_DATA')
-data = t.getcol('CORRECTED_DATA')
+#model = t.getcol('MODEL_DATA')
+#data = t.getcol('CORRECTED_DATA')
 
-subdata = data - model
+#subdata = data - model
 
-t.putcol('CORRECTED_DATA', subdata)
+#t.putcol('CORRECTED_DATA', subdata)
 
-t.close()
+#t.close()
 
-print '--- Subtracted MODEL DATA column in file ---'.format(toutname)
+#print '--- Subtracted MODEL DATA column in file ---'.format(toutname)
 
-print '--- NORMAL TERMINATION --- '
+#print '--- NORMAL TERMINATION --- '
 
 
 
